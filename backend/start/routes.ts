2608424536d3e91451fa4e8ee5966f95f86b7ca6
Pierre Cruz <contact@pierrecruz.com>
2024-04-03 12:17:10 +0200

    [WIP] Docker configuration

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,6 +7,7 @@
 |
 */
 
+const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/', [AuthController, 'index'])
