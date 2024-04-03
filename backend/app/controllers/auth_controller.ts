2be97a8e1bcb97a2805ea9f178ab9ec9ff45c2c4
Pierre Cruz <contact@pierrecruz.com>
2024-04-03 15:56:53 +0200

    [WIP] Authentification

--- backend/app/controllers/auth_controller.ts
+++ backend/app/controllers/auth_controller.ts
@@ -5,11 +5,13 @@ export default class AuthController {
     if (false) {
       return response.safeStatus(200)
     } else {
-      return response.redirect('/login')
+      return response.redirect('auth/login')
     }
   }
 
   async login(ctx: HttpContext) {
     return ctx.view.render('auth/login')
   }
+
+  async magicLink(ctx: HttpContext) {}
 }
